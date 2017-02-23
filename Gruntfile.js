52503c8481ac2dceb2de885cef43ab35736df28c
Guillermo Noain <guillermo.noain@quantion.com>
2017-02-23 10:50:29 +0100

    Runs unit tests using grunt

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
+
+  grunt.initConfig( {
+    mocha_istanbul: {
+      coverage: {
+        src:     [ 'server/**/*.spec.js' ],
+        options: {
+          coverageFolder: 'coverage/back',
+          excludes:       [ 'server/**/*.spec.js' ],
+        }
+      }
+    }
+  } );
+};
