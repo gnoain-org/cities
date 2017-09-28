7b377139fe58f3cad189e8a16ac23dc71e7578a4
Guillermo Noain <guillermo.noain@quantion.com>
2017-09-28 15:35:17 +0200

    fixes syntax error

--- server/api/cities/index.js
+++ server/api/cities/index.js
@@ -54,7 +54,7 @@ function hola() {
   return 'HEY!';
 }
 
-hola( 'HEY! );
+hola( 'HEY!' );
 
 function getAllCities( request, response ) {
     return responseSender.sendResponse( null, cities, response );
